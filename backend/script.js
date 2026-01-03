1de8da440dae72ab7c72fef9772f62ad69654363
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-01-03 13:57:47 +0530

    adding new files

---
+++ backend/script.js
@@ -0,0 +1,28 @@
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//     console.log("hello, ", i);
+// }
+
+// console.log("bye!");
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for (let i = 2; i < args.length; i++) {
+//     console.log("hello to ", args[i]);
+// }
+
+// const someValue = require("./math");
+
+// console.log(someValue);
+
+// const math = require("./math");
+
+// console.log(math);
+
+// to get the fruits info
+
+const info = require("./Fruits");
+console.log(info);
