6c4808c8dd8c375e545bbc02090a25a9c8f9aec2
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-01-08 23:59:30 +0530

    adding fikes

---
+++ backend/RestClass/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+       id : "1a",
+       username : "Dhruv Jungi",
+       content : "I Love Coding" 
+    },
+    {
+        id : "2b",
+        username : "Yug Gohel",
+        content : "I like bikes"
+    },
+    {  
+        id : "3c",
+        username : "Priyang Pabari",
+        content : "I like stockmarket"
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+    // res.send("serving working well!");
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({username, content });
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs");
+});
+
+app.listen(port, () => {
+    console.log("listening to port : 3000");
+});
