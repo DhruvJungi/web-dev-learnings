e59b4c511cf6179390893742928b33e3324f81ae
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-01-13 18:54:08 +0530

    restfull api completed

--- backend/RestClass/index.js
+++ backend/RestClass/index.js
@@ -4,8 +4,10 @@ const port = 3000;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
  // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -58,7 +60,19 @@ app.patch("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("edit.ejs");
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let  { id } = req.params;
+    posts = posts.filter((p) => id === p.id);
+    res.redirect("/posts");
 })
 
 app.listen(port, () => {
