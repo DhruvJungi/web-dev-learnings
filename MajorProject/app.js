dcbeadcbd0687fadc9ccff41a142fd5a1e8d728f
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-03-24 21:18:57 +0530

    project phase 1 complete

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,15 +21,66 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("hi, i am root");
 });
 
+//Index Route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
 });
+
+// /new Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// show route 
+app.get("/listings/:id", async (req, res) => {
+  let {id} = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/show.ejs", {listing});
+
+});
+
+// create route
+app.post("/listings", async (req, res) => {
+  try {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  } catch (err) {
+    console.error("Error saving listing:", err);
+  }
+});
+
+
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing }); 
+});
+
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");  
+});
+
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My New Villa",
